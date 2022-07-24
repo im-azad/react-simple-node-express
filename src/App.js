b6155ec5f86614fbfdf2d2e5b0e0bf892bd11cf8
Abul Kalam Azad <me.azad99@gmail.com>
2022-07-24 18:34:42 +0600

    sent data to server and receive data and show ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [users, setUsers] = useState([]);
+	const nameRef = useRef();
+	const emailRef = useRef();
 
+	useEffect(() => {
+		fetch('http://localhost:5000/users')
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const userName = nameRef.current.value;
+		const userEmail = emailRef.current.value;
+		const newUser = { name: userName, email: userEmail };
+		// send data to the server
+		fetch('http://localhost:5000/users', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newUser), // data sent to server
+		})
+			// data receive from server and show new data on ui
+
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				const addedUser = data;
+				const newUsers = [...users, addedUser];
+				setUsers(newUsers);
+			});
+		nameRef.current.value = '';
+		emailRef.current.value = '';
+	};
+	return (
+		<div className='App'>
+			<h1>Total User = {users.length}</h1>
+			<form onSubmit={handleSubmit}>
+				<input type='text' ref={nameRef} placeholder='name' />
+				<input type='email' ref={emailRef} placeholder='email' />
+				<input type='submit' value='Submit' />
+			</form>
+			{users.map((user) => (
+				<h4 key={user.id}>
+					id: {user.id}, Name: {user.name} , Email: {user.email}
+				</h4>
+			))}
+		</div>
+	);
+}
 export default App;
